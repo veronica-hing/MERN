75c50ab83c331d19bd9c75d57e19aa52851bc6b7
veronica-hing <veronica.m.hing@gmail.com>
2022-07-05 09:53:54 -0700

    bubble sort

---
+++ algos/july_05.js
@@ -0,0 +1,95 @@
+/* 
+  https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
+  Stable sort
+  
+  Time Complexity
+    - Best: O(n) linear when array is already sorted.
+    - Average: O(n^2) quadratic.
+    - Worst: O(n^2) quadratic when the array is reverse sorted.
+  Space: O(1) constant.
+  For review, create a function that uses BubbleSort to sort an unsorted array in-place.
+  For every pair of adjacent indices, swap them if the item on the left is larger than the item on the right until array is fully sorted
+*/
+
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Sorts the given nums in-place by mutating the array.
+ * Best: O(n) linear when array is already sorted.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic when the array is reverse sorted.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given nums after being sorted.
+ */
+function bubbleSort(nums) {
+
+    //iterate through, move first item in array and compare to others.
+    //stop when it's not greater than element to right of it
+    let swapHolder;
+    //outter for loop, is to repeat passes over entire length of array
+    for(let k = 0; k < nums.length; k++){
+        //this is the code for moving a number each pass
+        console.log(`on ${k}th pass`)
+        for(let i = 0; i < nums.length; i++){
+            if(nums[i]>nums[i+1]){
+                //swap shenanigans
+                swapHolder = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = swapHolder;
+                console.log(nums);
+            }
+        }
+    }
+    return nums;
+}
+//console.log(numsRandomOrder);
+//console.log(bubbleSort(numsRandomOrder));
+
+/* 
+  https://visualgo.net/en/sorting
+    
+  Selection sort works by iterating through the list, finding the minimum
+  value, and moving it to the beginning of the list. Then, ignoring the first
+  position, which is now sorted, iterate through the list again to find the
+  next minimum value and move it to index 1. This continues until all values
+  are sorted.
+  Unstable sort.
+  
+  Time Complexity
+    - Best: O(n^2) quadratic.
+    - Average: O(n^2) quadratic.
+    - Worst: O(n^2) quadratic.
+  Space: O(1) constant.
+  Selection sort is one of the slower sorts.
+  - ideal for: pagination, where page 1 displays 10 records for example,
+      you run selection sort for 10 iterations only to display the first 10
+      sorted items.
+*/
+
+// const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+// const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Sorts given array in-place.
+ * Best: O(n^2) quadratic.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given array after being sorted.
+ */
+function selectionSort(nums) {
+    let 
+    //iteration as a whole
+    for(let k = 0; k < nums.length; k++){
+        //swapping iteration
+        for(let i = 0; i < nums.length; i++){
+
+        }
+
+    }
+}
