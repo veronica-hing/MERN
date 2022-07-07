c5c63fa9643d24b285c7878ad0efa193b804be05
veronica-hing <veronica.m.hing@gmail.com>
2022-07-07 16:14:46 -0700

    implemented state to increment age.

--- homework/prop-it-up/src/PersonCard.js
+++ homework/prop-it-up/src/PersonCard.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: props.firstName,
+            lastName: props.lastName,
+            age: props.age,
+            hairColor: props.hairColor
+        }
+    }
+    makeOlder = () => {
+        this.setState({
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            age: this.state.age + 1,
+            hairColor: this.state.hairColor
+        });
+    }
+
     render() {
-        const { firstName, lastName, age, hairColor} = this.props;
         return(
             <div>
-                <h2>{lastName}, {firstName}</h2>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor}</p>
+                <h2>{this.state.lastName}, {this.state.firstName}</h2>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.state.hairColor}</p>
+                <button onClick={this.makeOlder}>Birthday button for {this.state.firstName} {this.state.lastName}</button>
             </div>
         )
     }
