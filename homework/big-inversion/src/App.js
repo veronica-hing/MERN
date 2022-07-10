a2a4ae10cce8b850bc4bcc7b2fa10c36ac8b32d3
veronica-hing <veronica.m.hing@gmail.com>
2022-07-10 16:04:38 -0700

    redid personCard with functional component.

---
+++ homework/big-inversion/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './personCard';
+
+function App() {
+  const persons = [
+    {
+      firstName: "Jane",
+      lastName: "Doe",
+      age: 45,
+      hairColor: "Black"
+    },
+    {
+      firstName: "John",
+      lastName: "Smith",
+      age: 88,
+      hairColor: "Brown"
+    },
+    {
+      firstName: "Millard",
+      lastName: "Fillmore",
+      age: 50,
+      hairColor: "Brown"
+    },
+    {
+      firstName: "Maria",
+      lastName: "Smith",
+      age: 62,
+      hairColor: "Brown"
+    }
+    ];
+
+    const peeps = persons.map( p => 
+      <PersonCard
+        firstName = {p.firstName}
+        lastName = {p.lastName}
+        age = {p.age}
+        hairColor = {p.hairColor}
+        >
+      </PersonCard>
+    )
+  return (
+    <div className="App">
+      {peeps}
+    </div>
+  );
+}
+
+export default App;
