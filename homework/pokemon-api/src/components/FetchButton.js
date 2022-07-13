5871edfa74902007f35bc1d4d09693450ae8fd7f
veronica-hing <veronica.m.hing@gmail.com>
2022-07-13 14:48:27 -0700

    added axios

--- homework/pokemon-api/src/components/FetchButton.js
+++ homework/pokemon-api/src/components/FetchButton.js
@@ -1,16 +1,17 @@
+import axios from 'axios';
+
 export const FetchButton = (props)=> {
     const fetchPokemons = () => {
         let pokeDex = [];//populate with names of pokemons
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
             .then(res => {
-                return res.json();
+                return res.data;
             })
             .then( res => {
                 res.results.map(pokemon =>{
                     pokeDex.push(pokemon.name);
                 });
                 props.setPokemons(pokeDex);
-                console.log(pokeDex);
             })
             .catch(err => {
                 console.log(err);
